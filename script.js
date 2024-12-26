b23c938dd0e6f71d8aa7a33690528db47c7bc87a
EmicJoykiller <152107345+EmicJoykiller@users.noreply.github.com>
2024-12-26 00:51:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,47 @@
-const taskForm = document.getElementById('task-form');
-const confirmCloseDialog = document.getElementById('confirm-close-dialog');
-const openTaskFormBtn = document.getElementById('open-task-form-btn');
+const taskForm = document.getElementById("task-form");
+const confirmCloseDialog = document.getElementById("confirm-close-dialog");
+const openTaskFormBtn = document.getElementById("open-task-form-btn");
+const closeTaskFormBtn = document.getElementById("close-task-form-btn");
+const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");
+const cancelBtn = document.getElementById("cancel-btn");
+const discardBtn = document.getElementById("discard-btn");
+const tasksContainer = document.getElementById("tasks-container");
+const titleInput = document.getElementById("title-input");
+const dateInput = document.getElementById("date-input");
+const descriptionInput = document.getElementById("description-input");
 
-const closeTaskFormBtn = document.getElementById('close-task-form-btn');
+const taskData = [];
+let currentTask = {};
 
-const addOrUpdateTaskBtn = document.getElementById('add-or-update-task-btn');
+openTaskFormBtn.addEventListener("click", () =>
+  taskForm.classList.toggle("hidden")
+);
 
-const cancelBtn = document.getElementById('cancel-btn');
+closeTaskFormBtn.addEventListener("click", () => {
+  confirmCloseDialog.showModal();
+});
 
-const discardBtn = document.getElementById('discard-btn');
-const tasksContainer = document.getElementById('tasks-container');
-const titleInput = document.getElementById('title-input');
+cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
+
+discardBtn.addEventListener("click", () => {
+  confirmCloseDialog.close();
+  taskForm.classList.toggle("hidden");
+});
+
+taskForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+  const taskObj = {
+    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    title: titleInput.value,
+    date: dateInput.value,
+    description: descriptionInput.value,
+  };
+
+  if (dataArrIndex === -1) {
+    taskData.unshift(taskObj);
+  }
+
+  taskData.forEach(({ id, title, date, description }) => {});
+});
